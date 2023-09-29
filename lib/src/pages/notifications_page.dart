66ed547fe09e0e83a375efa9319bc4933336c6e6
REV09 <hector.morelos.607@gmail.com>
2023-09-29 12:56:39 -0600

    Merge pull request #4 from REV09/david

--- lib/src/pages/notifications_page.dart
+++ lib/src/pages/notifications_page.dart
@@ -1,22 +1,98 @@
 import 'package:flutter/material.dart';
-
+import 'package:mibenv/src/pages/event_details_page.dart';
 import '../utils/responsive.dart';
 import 'widgets/notifications_card_landscape.dart';
 import 'widgets/notifications_card_portrait.dart';
 
-class notificationsPage extends StatefulWidget {
+class NotificationsPage extends StatefulWidget {
+  const NotificationsPage({super.key});
+
   @override
-  State<notificationsPage> createState() => _notificationsPage();
+  State<NotificationsPage> createState() => _NotificationsPage();
 }
 
-class _notificationsPage extends State<notificationsPage> {
+class _NotificationsPage extends State<NotificationsPage> {
+  List<SizedBox> eventsListPortrait = List.empty(growable: true);
+  List<SizedBox> eventsListLandscape = List.empty(growable: true);
+
   @override
   Widget build(BuildContext context) {
     String information = "Descripcion de prueba de informacion para la tarjeta "
-        "de notificaciones sobre eventos y avisos de la BENV";
+        " de notificaciones sobre eventos y avisos de la BENV de manera"
+        " extendida la idea es que la descripcion sea tan largan que no se pueda"
+        " visualizar de manera completa en la tarjeta tradicional de avisos y "
+        " en teoria esto ya deberia ser suficiente para poder ajustarlo a la"
+        " tarjeta y que no se pueda visualizar completo";
     String title = "Evento de prueba";
 
     Responsive responsive = Responsive(context);
+
+    eventsListPortrait.clear();
+    eventsListLandscape.clear();
+
+    eventsListPortrait.add(SizedBox(height: responsive.hp(8)));
+    eventsListLandscape.add(SizedBox(height: responsive.hp(8)));
+    for (int i = 0; i < 5; i++) {
+      NotificationCardLandscape eventCardLandscape = NotificationCardLandscape(
+        information: information,
+        title: title,
+        width: responsive.wp(70),
+      );
+
+      NotificationCardPortrait eventCardPortrait = NotificationCardPortrait(
+        information: information,
+        title: title,
+        width: responsive.wp(60),
+      );
+      eventCardPortrait.setMaxLines(6);
+      eventCardLandscape.setMaxLines(4);
+
+      eventsListPortrait.add(
+        SizedBox(
+          width: responsive.wp(90),
+          height: responsive.hp(32),
+          child: InkWell(
+            child: eventCardPortrait,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EventDetailsPage(
+                    title: title,
+                    information: information,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      );
+
+      eventsListLandscape.add(
+        SizedBox(
+          width: responsive.wp(60),
+          height: responsive.hp(60),
+          child: InkWell(
+            child: eventCardLandscape,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EventDetailsPage(
+                    title: title,
+                    information: information,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      );
+
+      eventsListPortrait.add(SizedBox(height: responsive.hp(2)));
+      eventsListLandscape.add(SizedBox(height: responsive.hp(2)));
+    }
+    eventsListPortrait.add(SizedBox(height: responsive.hp(5)));
+    eventsListLandscape.add(SizedBox(height: responsive.hp(5)));
+
     return Center(
       child: OrientationBuilder(
         builder: (context, orientation) => orientation == Orientation.portrait
@@ -28,71 +104,20 @@ class _notificationsPage extends State<notificationsPage> {
 
   SingleChildScrollView portraitView(
       String title, String information, Responsive responsive) {
-    List<SizedBox> eventsList = List.empty(growable: true);
-    eventsList.add(SizedBox(height: responsive.hp(8)));
-    for (int i = 0; i < 5; i++) {
-      eventsList.add(
-        SizedBox(
-          width: responsive.wp(90),
-          height: responsive.hp(32),
-          child: NotificationCardPortrait(
-            information: information,
-            title: title,
-            width: responsive.wp(60),
-            shapeBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-              side: const BorderSide(
-                color: Colors.blue,
-                width: 3,
-              ),
-            ),
-          ),
-        ),
-      );
-      eventsList.add(SizedBox(height: responsive.hp(2)));
-    }
-    eventsList.add(SizedBox(height: responsive.hp(5)));
-
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: eventsList,
+        children: eventsListPortrait,
       ),
     );
   }
 
   SingleChildScrollView landscapeView(
       String title, String information, Responsive responsive) {
-    List<SizedBox> eventList = List.empty(growable: true);
-    eventList.add(SizedBox(height: responsive.hp(8)));
-
-    for (int i = 0; i < 5; i++) {
-      eventList.add(
-        SizedBox(
-          width: responsive.wp(60),
-          height: responsive.hp(60),
-          child: NotificationCardLandscape(
-            information: information,
-            title: title,
-            width: responsive.wp(70),
-            shapeBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-              side: const BorderSide(
-                color: Colors.blue,
-                width: 3,
-              ),
-            ),
-          ),
-        ),
-      );
-      eventList.add(SizedBox(height: responsive.hp(2)));
-    }
-    eventList.add(SizedBox(height: responsive.hp(5)));
-
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: eventList,
+        children: eventsListLandscape,
       ),
     );
   }
